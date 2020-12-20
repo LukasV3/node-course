52671113e23cb82016accba9a4f76cd2a9516edc
LukasV3 <volk.lukas237@yahoo.co.uk>
2020-12-20 12:37:13 +0000

    added import-dev-data file where data gets imported into or deleted from the DB

--- natours-project/dev-data/data/import-dev-data.js
+++ natours-project/dev-data/data/import-dev-data.js
@@ -19,6 +19,37 @@ mongoose
   .then(() => console.log('DB connections successful'));
 
 // READ JSON FILE
-const tours = fs.readFileSync('tours-simple.json', 'utf-8');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+}
+if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
