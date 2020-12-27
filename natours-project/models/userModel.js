b405b1094dae3940c26e032f1c0909e9c05d07cd
LukasV3 <volk.lukas237@yahoo.co.uk>
2020-12-27 10:45:05 +0000

    implemented protected tour routes using protect func in authController

--- natours-project/models/userModel.js
+++ natours-project/models/userModel.js
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -41,7 +42,7 @@ userSchema.pre('save', async function (next) {
   // Hash the password with cost of 12
   this.password = await bcrytp.hash(this.password, 12);
 
-  // Delete passwordConfim field
+  // Delete passwordConfirm field
   this.passwordConfirm = undefined;
   next();
 });
@@ -51,6 +52,20 @@ userSchema.methods.correctPassword = async function (
   userPassword
 ) {
   return await bcrytp.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means NOT changed
+  return false;
 };
 
 const User = mongoose.model('User', userSchema);
