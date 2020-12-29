d5395d680b0f202a0568945d41124c44f74e6073
LukasV3 <volk.lukas237@yahoo.co.uk>
2020-12-29 13:12:41 +0000

    added missing authentication and authorization

--- natours-project/routes/userRoutes.js
+++ natours-project/routes/userRoutes.js
@@ -6,24 +6,19 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+// Protect all routes after this middleware
+router.use(authController.protect); // middlewares run in order so all routes below this one will be protected
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+// Protect all routes after this middleware
+router.use(authController.restrictTo('admin'));
 
 router
   .route('/')
