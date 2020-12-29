3a0bdab474b056647acac9f65b942c1d13b3e2e6
LukasV3 <volk.lukas237@yahoo.co.uk>
2020-12-29 11:24:35 +0000

    added factory functions along with nested routes

--- natours-project/routes/tourRoutes.js
+++ natours-project/routes/tourRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
+const reviewRouter = require('../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// NESTED ROUTES
+// POST /tour/:id/reviews
+// GET /tour/:id/reviews
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
